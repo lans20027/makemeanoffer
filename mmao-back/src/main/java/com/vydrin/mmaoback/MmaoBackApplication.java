87919ef9a8063caafcbc416c21bcb040007da94f
TFOMSNSO <evgeniy.alelekov@gmail.com>
2019-10-08 10:40:38 +0700

    init

---
+++ mmao-back/src/main/java/com/vydrin/mmaoback/MmaoBackApplication.java
@@ -0,0 +1,12 @@
+package com.vydrin.mmaoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaoBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmaoBackApplication.class, args);
+    }
+
+}
---
+++ mmao-back/src/main/java/com/vydrin/mmaoback/TestController.java
@@ -0,0 +1,13 @@
+package com.vydrin.mmaoback;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
